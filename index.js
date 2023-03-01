16aad4f9fa5dbf1fb00354ad22f50258b80f6e61
seancs50 <1.sean.fisher@gmail.com>
2023-03-01 12:01:26 +0800

    tidied up mongoose connections

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.static('build'))
 
-const mongoose = require('mongoose')
-url=process.env.MONGODB_URI
-mongoose.connect(url)
-console.log("URL",url)
-
 // dont forget this to handle json POST requests!!
 app.use(express.json())
 
@@ -21,7 +16,6 @@ const Person = require('./models/person')
 // define new token
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'));
-
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path:  ', request.path)
@@ -32,28 +26,7 @@ const requestLogger = (request, response, next) => {
   }
 app.use(requestLogger)
 
-let data  = [
-{ 
-  "id": 1,
-  "name": "Arto Hellas", 
-  "number": "040-123456"
-},
-{ 
-  "id": 2,
-  "name": "Ada Lovelace", 
-  "number": "39-44-5323523"
-},
-{ 
-  "id": 3,
-  "name": "Dan Abramov", 
-  "number": "12-43-234345"
-},
-{ 
-  "id": 4,
-  "name": "Mary Poppendieck", 
-  "number": "39-23-6423122"
-}
-]
+
 // DO NOT EVER FORGET THE LEADING FORWARD SLASH '/API....'
 app.get('/',(request,response) => {
     response.send('<h1>This is the Homepage!<h1>')
@@ -76,7 +49,6 @@ app.get('/api/persons/:id', (request, response, next) => {
   })
   .catch(error => next(error))
 })
-
 
 app.get("/info", (request,response, next) => {
   const date = new Date().toString()
@@ -103,10 +75,10 @@ app.get('/api/persons/:id', (request, response) => {
   
 })
 */
+
 app.put('/api/persons/:id',(request, response, next) => {
   console.log("called!")
   const body = request.body
-  
   const person = {
     name: body.name,
     number: body.number,
@@ -114,13 +86,10 @@ app.put('/api/persons/:id',(request, response, next) => {
   // [options.new=false] «Boolean» if true, return the modified document rather than the original
   Person.findByIdAndUpdate(request.params.id, person, { new: true })
     .then(updatedPerson => {
-      
       response.json(updatedPerson)
     })
     .catch(error => next(error))
 })
-
-
 
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
@@ -143,6 +112,7 @@ app.delete('/api/persons/:id', (request,response) => {
     response.status(204).end()
 })
 */
+
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(result => {
@@ -150,8 +120,6 @@ app.delete('/api/persons/:id', (request, response, next) => {
     })
     .catch(error => next(error))
 })
-
-const getRandomInt = (max)  => Math.floor(Math.random() * max)
 
 app.post('/api/persons', (request, response, next ) => {
     const body = request.body
@@ -174,15 +142,10 @@ app.post('/api/persons', (request, response, next ) => {
     entry.save().then(result => {
         console.log(`added ${body.name} number ${body.number} to phonebook`)
         console.log(JSON.stringify(result))
-        
-            response.json(result)
+          response.json(result)
           })
           .catch(error => next(error))
         })
-      
-        //mongoose.connection.close()
-    
-
 
     /*
     if (unique) {
@@ -191,7 +154,7 @@ app.post('/api/persons', (request, response, next ) => {
         })
     }
     
-
+    const getRandomInt = (max)  => Math.floor(Math.random() * max)
     id = getRandomInt(10000)
     name = body.name
     number = body.number
@@ -202,9 +165,9 @@ app.post('/api/persons', (request, response, next ) => {
     data = data.concat(person)//// this shoudda been in the front end
     console.log(data)
     response.json(data)
-    
 })
 */
+
 // this will only be called if there is no matching route...thats why it is at the bottom
 
 const unknownEndpoint = (request, response) => {
@@ -212,22 +175,18 @@ const unknownEndpoint = (request, response) => {
   }
 app.use(unknownEndpoint)
 
-
 //https://expressjs.com/en/guide/error-handling.html
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
-
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } 
-
   next(error)
 }
 
 // this has to be the last loaded middleware.
 app.use(errorHandler)
   
-
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () =>{
